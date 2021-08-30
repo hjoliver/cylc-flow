6cfd1a249f7473cd1b30f48468e9412085d58172
Hilary James Oliver <hilary.j.oliver@gmail.com>
2021-08-31 10:21:57 +1200

    Tidy up.

--- cylc/flow/cfgspec/workflow.py
+++ cylc/flow/cfgspec/workflow.py
@@ -1463,6 +1463,9 @@ def upg(cfg, descr):
         ('abort on timeout', 'abort on stall timeout'),
         ('timeout handler', 'stall timeout handler'),
         ('abort on stalled', 'abort on stall'),
+        ('inactivity', 'inactivity timeout'),
+        ('inactivity handler', 'inactivity timeout handler'),
+        ('aborted handler', 'abort handler'),
     ]:
         u.deprecate(
             '8.0.0',
